4d5a2a4ea07654749c9fc0eb222801589698f00d
halimocakli <halimcanocakli@gmail.com>
2024-11-07 16:22:18 +0300

    fix

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: "node",
   collectCoverage: true,
+  coverageDirectory: "coverage", // Ensure this is set
   coverageReporters: ["lcov"],
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
 };
